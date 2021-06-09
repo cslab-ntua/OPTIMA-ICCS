356470e11033a5324fab6b00abc037b9ede29fcc
Chloe Alverti <xalverti@cslab.ece.ntua.gr>
2021-06-09 11:57:44 +0300

    Added sketch code for most but not CG

---
+++ Sequential/Methods/sketch/LU/lu.c
@@ -0,0 +1,18 @@
+LU lu(Matrix matrix){
+    int n = matrix.n;
+    float **A = matrix.matrix;
+
+    for (int i = 0; i < n; i++) { // Iterates over the columns to remove
+        for(int j = i + 1; j < n; j++){
+            A[j][i] = A[j][i] / A[i][i]; // Computes the multipliers and updates L in A
+        }
+        for(int j = i + 1; j < n; j++){ // Iterates over the remaining rows
+            for(int k = i+1; k < n; k++){ // iterates over the remaining columns
+                A[j][k] -= A[j][i] * A[i][k]; // Updates U in A
+            }
+        }
+    }
+
+    LU decomposition = split_lu(A, n);
+    return decomposition;
+}
