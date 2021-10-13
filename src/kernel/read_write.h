664c890a78218fe05260480542e32b11760bcfe6
Panagiotis Miliadis <82807839+pmiliad@users.noreply.github.com>
2021-10-13 11:30:03 +0300

    Delete src directory

--- src/kernel/read_write.h
+++
@@ -1,6 +0,0 @@
-// Includes
-#include <ap_int.h>
-#include <hls_stream.h>
-
-static void read(float* in, hls::stream<float>& inStream, int size, int incx);
-static void write(float* out, hls::stream< float>& outStream, int size,int incy);
