9166f959f0bd23a33e432ec2a17a6d77181d683e
gpekridis <gpekridis@hotmail.com>
2024-01-14 17:52:10 +0200

    Update main_asum.cpp for code allignment
    No crucial changes just better code format

--- host/BLAS/L1/ASUM/main_asum.cpp
+++ host/BLAS/L1/ASUM/main_asum.cpp
@@ -4,34 +4,29 @@ void main_asum(){
 	int N=2048;
 	int NCU = 2;
 	int MAX_CUS = 16;
-    float* X;
-
-    double sum_sw = 0.0;
-    double sum_hw = 0.0;
-    double elapsed_krnl_time;
-
-    X=(float*)OOPS_malloc(sizeof(float)*N*incX);
-
-    std_vector_N(X, N, incX, 1.0);
-
-//verify
+	float* X;
+	
+	double sum_sw = 0.0;
+	double sum_hw = 0.0;
+	double elapsed_krnl_time;
+	
+	X=(float*)OOPS_malloc(sizeof(float)*N*incX);
+	std_vector_N(X, N, incX, 1.0);
+	
+	//verify
 	for(int i=0;i<N;i++){
-
 		sum_sw += X[i];
 	}
-
-   sum_hw = OOPS_asum( N, NCU, MAX_CUS, X, incX);
-
-//verify
+	
+	sum_hw = OOPS_asum( N, NCU, MAX_CUS, X, incX);
+	
+	//verify
 	int match = 0;
-
 	if ( (abs(sum_hw-sum_sw)>0.10) ) {
 		std::cout << "Error: Result mismatch" << std::endl;
 		match = 1;
-
 	}
-   	std::cout << "TEST " << (match ? "FAILED" : "PASSED") << std::endl;
-   
-   	free(X);
-
+	std::cout << "TEST " << (match ? "FAILED" : "PASSED") << std::endl;
+	
+	free(X);
 }
